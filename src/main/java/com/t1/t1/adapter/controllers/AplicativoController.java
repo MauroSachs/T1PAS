2aed94054a085350f7f6f5b84eb8b971b722b1a3
lucamandelli <luca.mandelli@poatek.com>
2024-06-03 18:27:33 -0300

    atualiza custo

--- src/main/java/com/t1/t1/adapter/controllers/AplicativoController.java
+++ src/main/java/com/t1/t1/adapter/controllers/AplicativoController.java
@@ -3,6 +3,7 @@ package com.t1.t1.adapter.controllers;
 import com.t1.t1.application.dtos.AplicativoDTO;
 import com.t1.t1.application.dtos.AplicativoRequestDTO;
 import com.t1.t1.application.usecases.aplicativo.AlterarAplicativoUseCase;
+import com.t1.t1.application.usecases.aplicativo.AtualizarCustoUseCase;
 import com.t1.t1.application.usecases.aplicativo.CreateAplicativoUseCase;
 import com.t1.t1.application.usecases.aplicativo.ListarAplicativosUseCase;
 import org.springframework.http.HttpStatus;
@@ -13,17 +14,29 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/aplicativo")
+@RequestMapping("/servcad/aplicativos")
 public class AplicativoController {
 
     private final CreateAplicativoUseCase createAplicativoUseCase;
     private final ListarAplicativosUseCase listarTodosAplicativosUseCase;
     private final AlterarAplicativoUseCase alterarAplicativoUseCase;
+    private final AtualizarCustoUseCase atualizarCustoUseCase;
 
-    public AplicativoController(CreateAplicativoUseCase createAplicativoUseCase, ListarAplicativosUseCase listarAplicativosUseCase, AlterarAplicativoUseCase alterarAplicativoUseCase) {
+    public AplicativoController(CreateAplicativoUseCase createAplicativoUseCase, ListarAplicativosUseCase listarTodosAplicativosUseCase, AlterarAplicativoUseCase alterarAplicativoUseCase, AtualizarCustoUseCase atualizarCustoUseCase) {
         this.createAplicativoUseCase = createAplicativoUseCase;
-        this.listarTodosAplicativosUseCase = listarAplicativosUseCase;
+        this.listarTodosAplicativosUseCase = listarTodosAplicativosUseCase;
         this.alterarAplicativoUseCase = alterarAplicativoUseCase;
+        this.atualizarCustoUseCase = atualizarCustoUseCase;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<AplicativoDTO>> listarTodos() {
+        try {
+            List<AplicativoDTO> aplicativos = listarTodosAplicativosUseCase.call();
+            return ResponseEntity.ok(aplicativos);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -36,15 +49,18 @@ public class AplicativoController {
         }
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<AplicativoDTO>> listarTodos() {
+    //atualiza custo
+    @PostMapping(value = "/atualizacusto/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<AplicativoDTO> atualizaCusto(@PathVariable Long id, @RequestBody AplicativoRequestDTO request) {
         try {
-            List<AplicativoDTO> aplicativos = listarTodosAplicativosUseCase.call();
-            return ResponseEntity.ok(aplicativos);
+            AplicativoDTO aplicativoDTO = atualizarCustoUseCase.call(id, request.custoMensal());
+            return ResponseEntity.ok(aplicativoDTO);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+
 
     @PatchMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AplicativoDTO> alterar(@PathVariable Long id, @RequestBody AplicativoRequestDTO request) {
